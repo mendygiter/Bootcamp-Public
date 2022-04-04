c330e90a398a50f3c275162b8b26d18a8bedc909
mendygiter <87391023+mendygiter@users.noreply.github.com>
2022-04-04 14:05:18 -0400

    Add files via upload

---
+++ Giter_Mendy_JS_Tinkering.js
@@ -0,0 +1,11 @@
+var childHeight = 52;
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight) {
+}
+if (childHeight => 52) {
+console.log("Get on that ride, kiddo!");
+}
+else {
+console.log ("Sorry kiddo. Maybe next year.");
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster();
